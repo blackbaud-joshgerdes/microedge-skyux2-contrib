a043450fc5b4ad6d759e82859987f1e6d0fc76c2
Josh Gerdes <josh.gerdes@blackbaud.me>
2017-05-19 11:02:59 -0500

    Update docs for link-records

--- demo/src/link-records/app.component.ts
+++ demo/src/link-records/app.component.ts
@@ -32,7 +32,7 @@ class AppComponent {
 
   public matchFields: Array<string> = ['description', 'name'];
 
-  public newItem: any = { id: '69', address: 6969, name: 'Kiwi', description: 'Kathy eats kiwis.' };
+  public newItem: any = { id: '99', address: 999, name: 'Lime', description: 'Laura eats limes.' };
 
   public items: Observable<any> = Observable.of([
     { id: '1', address: 101, name: 'Apple', description: 'Anne eats apples' },
@@ -41,39 +41,45 @@ class AppComponent {
     { id: '4', address: 404, name: 'Grape', description: 'George eats grapes' },
     { id: '5', address: 505, name: 'Banana', description: 'Becky eats bananas' },
     { id: '6', address: 606, name: 'Lemon', description: 'Larry eats lemons' },
-    { id: '7', address: 707, name: 'Strawberry', description: 'Sally eats strawberries' }
+    { id: '7', address: 707, name: 'Kiwi', description: 'Kim eats kiwis.' },
+    { id: '8', address: 808, name: 'Strawberry', description: 'Sally eats strawberries' }
   ]);
 
   public matches: Observable<Array<LinkRecordsMatchModel>> = Observable.of([
     new LinkRecordsMatchModel({
       key: '1',
+      status: Statuses.Edit,
+      item: { id: '11', address: 111, name: 'Big Apple', description: 'George and his apples' }
+    }),
+    new LinkRecordsMatchModel({
+      key: '2',
       status: null,
       item: null
     }),
     new LinkRecordsMatchModel({
-      key: '2',
-      status: Statuses.Edit,
-      item: { id: '22', address: 111, name: 'Big Apple', description: 'George and his apples' }
-    }),
-    new LinkRecordsMatchModel({
       key: '3',
-      status: Statuses.Suggested,
-      item: { id: '11', address: 333, name: 'Perfect Pear', description: 'Peach loves pears' }
-    }),
-    new LinkRecordsMatchModel({
-      key: '4',
-      status: Statuses.Created,
-      item: { id: '44', address: 777, name: 'Grape Ape', description: 'Jane loves bananas' }
-    }),
-    new LinkRecordsMatchModel({
-      key: '5',
       status: Statuses.NoMatch,
       item: null
     }),
     new LinkRecordsMatchModel({
+      key: '5',
+      status: Statuses.Suggested,
+      item: { id: '55', address: 555, name: 'Huge Banana', description: 'Barry loves bananas.' }
+    }),
+    new LinkRecordsMatchModel({
+      key: '6',
+      status: Statuses.Selected,
+      item: { id: '66', address: 666, name: 'Lovely Lemons', description: 'Lisa loves lemons.' }
+    }),
+    new LinkRecordsMatchModel({
       key: '7',
+      status: Statuses.Created,
+      item: null
+    }),
+    new LinkRecordsMatchModel({
+      key: '8',
       status: Statuses.Linked,
-      item: { id: '77', address: 999, name: 'Strawberry Shortcake', description: 'Steve loves strawberries' }
+      item: { id: '88', address: 888, name: 'Strawberry Shortcake', description: 'Steve loves strawberries' }
     })
   ]);
 }
